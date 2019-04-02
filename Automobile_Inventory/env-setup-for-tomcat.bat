01b36f74cd5ae43af11c2293516682913f62c9b4
abishekkrish <avellineni@hawk.iit.edu>
2019-04-01 20:08:06 -0500

    Automobile Inventory

---
+++ Automobile_Inventory/env-setup-for-tomcat.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_65
+set PATH="C:\Program Files\Java\jdk1.8.0_65\bin";%PATH%
+set CLASSPATH=.;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\servlet-api.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\jsp-api.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\el-api.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\mysql-connector-java-5.1.47-bin.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\mongo-java-driver-3.2.2.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\gson-2.3.1.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\mail.jar;C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34\lib\activation.jar
+set ANT_HOME=C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34
+set TOMCAT_HOME=C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34
+set CATALINA_HOME=C:\tomcat-7.0.34-preconfigured\apache-tomcat-7.0.34
